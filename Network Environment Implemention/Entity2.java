e8792444772758abee7ebe25dd3d72f0118ec8cd
GeorgeAria <george.arias94@yahoo.com>
2017-11-18 08:42:08 -0800

    Add files via upload

---
+++ Network Environment Implemention/Entity2.java
@@ -0,0 +1,148 @@
+public class Entity2 extends Entity
+{    
+    // Perform any necessary initialization in the constructor
+    public Entity2()
+    {
+    	for(int i = 0; i < NetworkSimulator.NUMENTITIES; i++)
+    	{
+    		for(int j = 0; j < NetworkSimulator.NUMENTITIES; j++)
+    		{
+    			if (i == 2 || i == j)
+    			{
+    				distanceTable[i][j] = NetworkSimulator.cost[i][j];
+    			}
+    			else
+    			{
+    				distanceTable[i][j] = 999;
+    			}
+    		}
+    	}
+    	
+    	printDT();
+    	
+    	for(int i = 0; i < NetworkSimulator.NUMENTITIES; i++)
+    	{
+    		if (i != 2)
+    		{
+    			if (NetworkSimulator.cost[2][i] != 999)
+    			{
+    				int[] copy = new int[NetworkSimulator.NUMENTITIES];
+    				for(int x = 0; x < NetworkSimulator.NUMENTITIES; x++)
+    				{
+    					copy[x] = distanceTable[2][x];
+    				}
+    				
+    				Packet p = new Packet(2, i, copy);
+    				NetworkSimulator.toLayer2(p);
+    			}
+    		}
+    	}
+    }
+    
+    // Handle updates when a packet is received.  Students will need to call
+    // NetworkSimulator.toLayer2() with new packets based upon what they
+    // send to update.  Be careful to construct the source and destination of
+    // the packet correctly.  Read the warning in NetworkSimulator.java for more
+    // details.
+    public void update(Packet p)
+    {
+    	System.out.println("------------------------------------");
+    	System.out.println("The beginning of update() in node " + String.valueOf(p.getDest()));
+    	System.out.println("------------------------------------");
+    	System.out.println();
+    	printDT();
+    	
+    	for(int i = 0; i < NetworkSimulator.NUMENTITIES; i++)
+    	{
+    		distanceTable[p.getSource()][i] = p.getMincost(i);
+    	}
+    	
+    	int cost = 0;
+        boolean changed = false;
+    	for(int x = 0; x < NetworkSimulator.NUMENTITIES; x++)
+    	{
+    		for(int y = 0; y < NetworkSimulator.NUMENTITIES; y++)
+    		{
+    			cost = distanceTable[x][y];
+    			
+    			if(x != 2)
+    			{
+    				cost += distanceTable[2][x];
+    			}
+    			
+    			if(cost < NetworkSimulator.cost[2][y])
+    			{
+    				NetworkSimulator.cost[2][y] = cost;
+    			    changed = true;
+    			}
+    		}
+    	}
+    	
+    	System.out.println("------------------------------------");
+    	System.out.println("The end of update() in node " + String.valueOf(p.getDest()));
+    	System.out.println("------------------------------------");
+    	System.out.println();
+    	
+    	if(changed)
+    	{
+    		printDT();
+    		System.out.println("Distance Vector Updated!");
+    		for(int i = 0; i < NetworkSimulator.NUMENTITIES; i++)
+        	{
+        		if (i != 2)
+        		{
+        			if (NetworkSimulator.cost[2][i] != 999)
+        			{
+        				int[] copy = new int[NetworkSimulator.NUMENTITIES];
+        				for(int x = 0; x < NetworkSimulator.NUMENTITIES; x++)
+        				{
+        					copy[x] = distanceTable[2][x];
+        				}
+        				
+        				Packet k = new Packet(2, i, copy);
+        				NetworkSimulator.toLayer2(k);
+        			}
+        		}
+        	}
+    	}
+    	else
+    	{
+    		printDT();
+    		System.out.println("No change!!");
+    	}
+    }
+    
+    public void linkCostChangeHandler(int whichLink, int newCost)
+    {
+    }
+    
+    public void printDT()
+      {
+          System.out.println();
+          System.out.println("           via");
+          System.out.println(" D2 |  0   1   2   3");
+          System.out.println("----+-----------------");
+          for (int i = 0; i < NetworkSimulator.NUMENTITIES; i++)
+          {
+              System.out.print("   " + i + "|");
+              for (int j = 0; j < NetworkSimulator.NUMENTITIES; j++)
+              {
+                  if (distanceTable[i][j] < 10)
+                  {
+                      System.out.print("   ");
+                  }
+                  else if (distanceTable[i][j] < 100)
+                  {
+                      System.out.print("  ");
+                  }
+                  else
+                  {
+                      System.out.print(" ");
+                  }
+
+                  System.out.print(distanceTable[i][j]);
+              }
+              System.out.println();
+          }
+      }
+}
